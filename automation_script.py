9c08138daa046b6af366e6f71ec357c227c41530
luciano.moraes <lucianomm98@gmail.com>
2024-04-26 19:55:40 -0400

    code formatting

--- automation_script.py
+++ automation_script.py
@@ -6,6 +6,31 @@ import os
 from scipy.stats import linregress
 import numpy as np
 
+# Constants
+pellet_file_id_index_start = 12
+pellet_file_id_index_end = 13
+
+time_column_title = 'Time(s)'
+weight_column_title = 'Weight'
+oxygen_in_pellet_title = 'Oxygen_Pct'
+reduction_title = 'Reduction'
+reduction_pct_title = 'Reduction_Pct'
+iron_layer_limiting_title = 'Iron_Layer_LRE'
+limiting_mixed_control_title = 'Mixed_Control_LRE'
+complete_internal_burning_title = 'Complete_Internal_Burning'
+
+max_time_plot_s = 25*60
+experiment_duration = 100*60
+plateau_time = 3*60
+hematite_oxygen_pct = 0.300564
+hematite_iron_pct = 0.699436
+
+# Figure setup
+fig1, reduct_graph = plt.subplots(figsize=(10, 6))
+fig2, iron_layer_limiting_graph = plt.subplots(figsize=(10, 6))
+fig3, mixed_control_limiting_graph = plt.subplots(figsize=(10, 6))
+fig4, complete_internal_burning_graph = plt.subplots(figsize=(10, 6))
+
 class LinregressRange:
     def __init__(self, min, max) -> None:
         self.min = min
@@ -66,8 +91,8 @@ class Pellet:
             alpha=1
         )
 
-def convert_to_pellet_config(pellet_number) -> Pellet:
-    #Information Pellet 1
+def convert_to_pellet_config(pellet_number:int) -> Pellet:
+    #Information Pellet 1W
     pellet_1 = Pellet(
         initial_mass=9.695,
         start_time_s=4670,
@@ -109,151 +134,135 @@ def convert_to_pellet_config(pellet_number) -> Pellet:
         9: pellet_9,
         10: pellet_10,
     }
+    if pellet_number not in switcher:
+        raise ValueError(f"No file found for Pellet number: {pellet_number}")
  
     # get() method of dictionary data type returns value of passed argument if it is present in dictionary otherwise second argument will be assigned as default value of passed argument
-    return switcher.get(pellet_number)
+    return switcher.get(pellet_number) # type: ignore
 
-# config variables
-pellet_file_id_index_start = 12
-pellet_file_id_index_end = 13
-
-time_column_title = 'Time(s)'
-weight_column_title = 'Weight'
-oxygen_in_pellet_title = 'Oxygen_Pct'
-reduction_title = 'Reduction'
-reduction_pct_title = 'Reduction_Pct'
-iron_layer_limiting_title = 'Iron_Layer_LRE'
-limiting_mixed_control_title = 'Mixed_Control_LRE'
-complete_internal_burning_title = 'Complete_Internal_Burning'
-
-max_time_plot_s = 25*60
-experiment_duration = 100*60
-plateau_time = 3*60
-hematite_oxygen_pct = 0.300564
-hematite_iron_pct = 0.699436
-
-# Get the script's current directory
-dir_current = os.path.dirname(os.path.abspath(__file__))
-
-# Lists all files in the directory
-files_in_folder = os.listdir(dir_current)
-
-# List to store the DataFrames of each file
-data_temp = {}
-data_mass_dict: Dict[str,pd.DataFrame] = {} # dict with file name as keys and DataFrame as values
-
-# Iterates over the files in the folder
-for file_name in files_in_folder:
-    # Check if the file is a CSV or TXT file
-    if file_name.endswith('.csv') or file_name.endswith('.txt'):
-        # Assemble the full file path
-        full_path = os.path.join(dir_current, file_name)
-
-        if file_name.endswith('.csv'):
-            # # Reads the file skipping the first 8 lines and taking ';' as a separator (adjust as necessary)
-            # data_temp.append(pd.read_csv(caminho_completo, skiprows=8, on_bad_lines='skip'))
-            pass
-        else:
-            file_name_without_txt = os.path.splitext(file_name)[0]
-            data_mass_dict[file_name_without_txt] = pd.read_csv(full_path, sep='\t')
-
-###Filtrar
-
-# prep figure
-fig1, reduct_graph = plt.subplots(figsize=(10, 6))
-fig2, iron_layer_limiting_graph = plt.subplots(figsize=(10, 6))
-fig3, mixed_control_limiting_graph = plt.subplots(figsize=(10, 6))
-fig4, complete_internal_burning_graph = plt.subplots(figsize=(10, 6))
-
-for file_name, file_data in data_mass_dict.items():
+def convert_file_name_to_pellet_config(file_name:str) -> Pellet:
     pellet_number = int(file_name[pellet_file_id_index_start:pellet_file_id_index_end+1])
-    pellet_config = convert_to_pellet_config(pellet_number)
+    return convert_to_pellet_config(pellet_number)
+
+# helper methods
+def read_data_files(path: str) -> Dict[str, pd.DataFrame]:
+    data_mass_dict: Dict[str, pd.DataFrame] = {}  # dict with file name as keys and DataFrame as values
+
+    # Lists all files in the directory
+    files_in_folder = os.listdir(path)
+
+    # Iterates over the files in the folder
+    for file_name in files_in_folder:
+        # Check if the file is a CSV or TXT file
+        if file_name.endswith('.csv') or file_name.endswith('.txt'):
+            # Assemble the full file path
+            full_path = os.path.join(path, file_name)
+
+            if file_name.endswith('.csv'):
+                # # Reads the file skipping the first 8 lines and taking ';' as a separator (adjust as necessary)
+                # data_temp.append(pd.read_csv(caminho_completo, skiprows=8, on_bad_lines='skip'))
+                pass
+            else:
+                file_name_without_txt = os.path.splitext(file_name)[0]
+                data_mass_dict[file_name_without_txt] = pd.read_csv(full_path, sep='\t')
+
+    return data_mass_dict
+
+def format_file_data(file_data:DataFrame, pellet_config:Pellet) -> DataFrame:
     formatted_data:DataFrame = file_data.iloc[:, :2]
     formatted_data.columns = [time_column_title,weight_column_title]
 
-    # exclude initial values and set the new initial time as zero
+        # exclude initial values and set the new initial time as zero
     formatted_data = formatted_data.iloc[pellet_config.start_time_s:]
     formatted_data[time_column_title] -= pellet_config.start_time_s
 
-    # exclude final values
+        # exclude final values
     formatted_data = formatted_data.iloc[:experiment_duration]
- 
-    # Oxygen percentage in pellet = (weight + oxygen)/(weight + oxygen + iron)
+    
+        # Oxygen percentage in pellet = (weight + oxygen)/(weight + oxygen + iron)
     formatted_data[oxygen_in_pellet_title] = (formatted_data[weight_column_title] + pellet_config.oxy) / (formatted_data[weight_column_title] + pellet_config.oxy + pellet_config.iron)
 
-    # # Reduction = ((Ox / Fe)[fe2o3] - (Ox / Fe)[DRI]) / (Ox / Fe)[fe2o3]
-    # formatted_data[reduction_title] = ((hematite_oxygen_pct / hematite_iron_pct) - (formatted_data[oxygen_in_pellet_title]/hematite_iron_pct)) / (hematite_oxygen_pct / hematite_iron_pct)
-    
+        # # Reduction = ((Ox / Fe)[fe2o3] - (Ox / Fe)[DRI]) / (Ox / Fe)[fe2o3]
+        # formatted_data[reduction_title] = ((hematite_oxygen_pct / hematite_iron_pct) - (formatted_data[oxygen_in_pellet_title]/hematite_iron_pct)) / (hematite_oxygen_pct / hematite_iron_pct)
+        
 
-    # # formatted_data[reduction_pct_title] = formatted_data[weight_column_title] delta * 97%
-    # # Reduction Percentage = ((Ox / Fe)[fe2o3] - (Ox / Fe)[DRI]) * 100 / (Ox / Fe)[fe2o3]
-    # formatted_data[reduction_pct_title] = formatted_data[reduction_title] * 100
+        # # formatted_data[reduction_pct_title] = formatted_data[weight_column_title] delta * 97%
+        # # Reduction Percentage = ((Ox / Fe)[fe2o3] - (Ox / Fe)[DRI]) * 100 / (Ox / Fe)[fe2o3]
+        # formatted_data[reduction_pct_title] = formatted_data[reduction_title] * 100
 
-    # Correcting reduction curves from XRD result
-    # plateau_value = formatted_data[weight_column_title].tail(plateau_time).min()
-    # formatted_data[weight_column_title] = formatted_data[weight_column_title].rolling(window=10).mean()
+        # Correcting reduction curves from XRD result
+        # plateau_value = formatted_data[weight_column_title].tail(plateau_time).min()
+        # formatted_data[weight_column_title] = formatted_data[weight_column_title].rolling(window=10).mean()
     plateau_value = formatted_data[weight_column_title].tail(plateau_time).median()
     formatted_data[reduction_title] = (formatted_data[weight_column_title]/plateau_value) * pellet_config.iron_content_XRD
     formatted_data[reduction_pct_title] = formatted_data[reduction_title]*100
-        
     
-    # Plot reduction percentage curve
-    pellet_config.plot(
-        graph=reduct_graph,
-        time_data_s=formatted_data[time_column_title].iloc[:max_time_plot_s],
-        data=formatted_data[reduction_title].iloc[:max_time_plot_s],
-        # linregress_ranges=[
-        #     LinregressRange(min=40, max=50),
-        #     LinregressRange(min=50, max=60),
-        #     LinregressRange(min=60, max=70),
-        #     LinregressRange(min=70, max=80),
-        #     LinregressRange(min=80, max=90),
-        #     ]
-    )
-
     # Fit the rate modlesd
     formatted_data[iron_layer_limiting_title] = 1/2-(1/3)*formatted_data[reduction_title]-(1/2)*pow(1-formatted_data[reduction_title],2/3)
     formatted_data[limiting_mixed_control_title] = 1-pow(1-formatted_data[reduction_title],1/3)
     formatted_data[complete_internal_burning_title] = np.log(1-formatted_data[reduction_title])
+        
+    return formatted_data
 
-    # Plot both equations (iron layer limiting rate equation curve + reduction)
-    pellet_config.plot(
-        graph=iron_layer_limiting_graph,
-        time_data_s=formatted_data[time_column_title].iloc[:max_time_plot_s],
-        data=formatted_data[iron_layer_limiting_title].iloc[:max_time_plot_s]
-    )
-    #Plot equation of mixed control limiting rate equation curve
-    pellet_config.plot(
-        graph=mixed_control_limiting_graph,
-        time_data_s=formatted_data[time_column_title].iloc[:max_time_plot_s],
-        data=formatted_data[limiting_mixed_control_title].iloc[:max_time_plot_s]
-    )
+def plot_data_points(experiment_data_files:DataFrame):
+    
+    for file_name, file_data in experiment_data_files.items():
+        pellet_config = convert_file_name_to_pellet_config(file_name)
+        formatted_data = format_file_data(file_data, pellet_config)
 
-    #Plot equation of mixed control limiting rate equation curve
-    pellet_config.plot(
-        graph=complete_internal_burning_graph,
-        time_data_s=formatted_data[time_column_title].iloc[:max_time_plot_s],
-        data=formatted_data[complete_internal_burning_title].iloc[:max_time_plot_s]
-    )
-    # ax.plot(xlingress/60, predicted_Y, color='gray', label='linear fitting' + pellet_config.label)
+        # Plot reduction percentage curve
+        pellet_config.plot(
+            graph=reduct_graph,
+            time_data_s=formatted_data[time_column_title].iloc[:max_time_plot_s],
+            data=formatted_data[reduction_title].iloc[:max_time_plot_s],
+        )
 
-#plt.grid(True)
-reduct_graph.set_xlabel('Time (min)', color = "black", fontsize = 12)
-reduct_graph.set_ylabel('F', color = "black", fontsize = 12)
+        # Plot both equations (iron layer limiting rate equation curve + reduction)
+        pellet_config.plot(
+            graph=iron_layer_limiting_graph,
+            time_data_s=formatted_data[time_column_title].iloc[:max_time_plot_s],
+            data=formatted_data[iron_layer_limiting_title].iloc[:max_time_plot_s]
+        )
 
-iron_layer_limiting_graph.set_xlabel('Time (min)', color = "black", fontsize = 12)
-iron_layer_limiting_graph.set_ylabel('$\\frac{1}{2}-\\frac{1}{3}F-\\frac{1}{2}(1-F)^{\\frac{2}{3}}$', color="black", fontsize=12)
+        # Plot equation of mixed control limiting rate equation curve
+        pellet_config.plot(
+            graph=mixed_control_limiting_graph,
+            time_data_s=formatted_data[time_column_title].iloc[:max_time_plot_s],
+            data=formatted_data[limiting_mixed_control_title].iloc[:max_time_plot_s]
+        )
 
-mixed_control_limiting_graph.set_xlabel('Time (min)', color = "black", fontsize = 12)
-mixed_control_limiting_graph.set_ylabel('$1-(1-F)^\\frac{1}{3}$', color = "black", fontsize = 12)
+        # Plot equation of mixed control limiting rate equation curve
+        pellet_config.plot(
+            graph=complete_internal_burning_graph,
+            time_data_s=formatted_data[time_column_title].iloc[:max_time_plot_s],
+            data=formatted_data[complete_internal_burning_title].iloc[:max_time_plot_s]
+        )
 
-complete_internal_burning_graph.set_xlabel('Time (min)', color = "black", fontsize = 12)
-complete_internal_burning_graph.set_ylabel('$ln(1-F)$', color = "black", fontsize = 12)
 
-# Adicione uma legenda
-reduct_graph.legend(loc = 'lower right')
-iron_layer_limiting_graph.legend(loc = 'lower right')
-mixed_control_limiting_graph.legend(loc = 'lower right')
-complete_internal_burning_graph.legend(loc = 'lower right')
+def plot_data(experiment_data_files: Dict[str, pd.DataFrame]):
 
+    plot_data_points(experiment_data_files)
+
+    reduct_graph.set_xlabel('Time (min)', color="black", fontsize=12)
+    reduct_graph.set_ylabel('F', color="black", fontsize=12)
+
+    iron_layer_limiting_graph.set_xlabel('Time (min)', color="black", fontsize=12)
+    iron_layer_limiting_graph.set_ylabel('$\\frac{1}{2}-\\frac{1}{3}F-\\frac{1}{2}(1-F)^{\\frac{2}{3}}$', color="black",
+                                        fontsize=12)
+
+    mixed_control_limiting_graph.set_xlabel('Time (min)', color="black", fontsize=12)
+    mixed_control_limiting_graph.set_ylabel('$1-(1-F)^\\frac{1}{3}$', color="black", fontsize=12)
+
+    complete_internal_burning_graph.set_xlabel('Time (min)', color="black", fontsize=12)
+    complete_internal_burning_graph.set_ylabel('$ln(1-F)$', color="black", fontsize=12)
+
+    reduct_graph.legend(loc='lower right')
+    iron_layer_limiting_graph.legend(loc='lower right')
+    mixed_control_limiting_graph.legend(loc='lower right')
+    complete_internal_burning_graph.legend(loc='lower right')
+
+# Get the data from files
+dir_current = os.path.dirname(os.path.abspath(__file__))
+experiment_data_files = read_data_files(dir_current)
+plot_data(experiment_data_files)
 plt.show()
